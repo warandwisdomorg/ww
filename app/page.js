373c798c80b2f2c4bdb11ed96053523a102d0d64
Can Cömertpay <cancomertpay@gmail.com>
2023-09-06 23:14:27 +0300

    #Add ScrollToTop component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import { Footer, Navbar } from '../components';
+import { ScrollToTop } from '../components/ScrollToTop';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
   <div className='bg-primary-blue overflow-hidden'>
+    <ScrollToTop />
     <Navbar />
     <Hero />
     <About />
---
+++ components/ScrollToTop.jsx
@@ -0,0 +1,49 @@
+'use client'
+
+
+import { useEffect, useState } from 'react'
+import { BiArrowFromBottom } from 'react-icons/bi'
+
+import { classNames } from '../utils/classnames'
+
+export const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
+  return (
+    <div className="fixed bottom-12 right-12 z-20">
+      <button
+        type="button"
+        onClick={scrollToTop}
+        className={classNames(
+          isVisible ? 'opacity-100' : 'opacity-0',
+          'bg-secondary-white hover:bg-primary-blue focus:ring-blue-950 inline-flex items-center rounded-full p-3 text-black shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2',
+        )}
+      >
+        <BiArrowFromBottom className="h-6 w-6" aria-hidden="true" />
+      </button>
+    </div>
+  )
+}
